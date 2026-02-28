1f25bb78074109cc522df2e434a575bebe277e71
Deepanshu-dashore <dashd9396@gmail.com>
2026-02-28 14:36:00 +0530

    feat: Add warehouse order and distributor detail pages, along with a new portal wrapper component.

--- src/components/warehouse/WarehousePortalWrapper.jsx
+++ src/components/warehouse/WarehousePortalWrapper.jsx
@@ -12,9 +12,9 @@ export default function WarehousePortalWrapper({ children }) {
     }
 
     return (
-        <div className="flex min-h-screen bg-gray-50/50">
+        <div className="flex min-h-screen">
             <WarehouseSidebar />
-            <main className="flex-1 lg:overflow-y-auto h-screen pl-64 pt-16 lg:pt-0">
+            <main className="flex-1 lg:overflow-y-auto h-screen pl-64 pt-16 lg:pt-0  bg-linear-to-t from-blue-400/5 to-primary/5">
                 {children}
             </main>
         </div>
