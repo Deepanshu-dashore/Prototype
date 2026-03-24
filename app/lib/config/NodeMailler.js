114b86b1c357681e73da9e377508e0f097d83869
harshraj <harshrajrathore.dev@gmail.com>
2026-03-24 20:39:06 +0530

    nodemailer updated

--- app/lib/config/NodeMailler.js
+++ app/lib/config/NodeMailler.js
@@ -1,7 +1,7 @@
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
   port: 2525,
   secure: false,
   auth: {
